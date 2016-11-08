0361529f65a9d0bb4d3180702bea278e4d5cb3cd
PiotrJander <pjander2@gmail.com>
2016-11-08 13:18:50 -0500

    Displays workers in /status

--- src/edu/upenn/cis455/mapreduce/master/MasterServlet.java
+++ src/edu/upenn/cis455/mapreduce/master/MasterServlet.java
@@ -3,6 +3,7 @@ package edu.upenn.cis455.mapreduce.master;
 import java.io.*;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,7 +15,10 @@ public class MasterServlet extends HttpServlet {
     private HttpServletRequest request;
     private HttpServletResponse response;
 
+    private Map<String, Map<String, Object>> workers = new HashMap<>();
+
     private static class WorkerFields {
+        final static String ID = "id";
         final static String IP = "ip";
         final static String LAST_ACTIVE = "lastActive";
         final static String PORT = "port";
@@ -23,9 +27,11 @@ public class MasterServlet extends HttpServlet {
         final static String KEYS_READ = "keysRead";
         final static String KEYS_WRITTEN = "keysWritten";
         final static String RESULTS = "results";
-        
+
         final static Stream<String> requestsParams = Stream.of(PORT, STATUS, JOB, KEYS_READ, KEYS_WRITTEN, RESULTS);
     }
+
+    private final int ACTIVE_DURATION = 30;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws java.io.IOException {
         this.request = request;
@@ -47,7 +53,21 @@ public class MasterServlet extends HttpServlet {
         writer.println("<table>");
         writer.println("<thead><tr><th>IP:port</th><th>status</th><th>job</th><th>keys read</th><th>keys written</th></tr></thead>");
         writer.println("<tbody>");
-        writer.println("<tr><td>ipport</td><td>status</td><td>job</td><td>keys read</td><td>keys written</td></tr>");
+
+        workers.forEach((k, worker) -> {
+            Instant lastActive = (Instant) worker.get(WorkerFields.LAST_ACTIVE);
+            if (lastActive.isBefore(Instant.now().minusSeconds(ACTIVE_DURATION))) {
+                writer.format(
+                        "<tr><td>%s</td><td>%s</td><td>%s</td><td>%d</td><td>%d</td></tr>\n",
+                        worker.get(WorkerFields.ID),
+                        worker.get(WorkerFields.STATUS),
+                        worker.get(WorkerFields.JOB),
+                        worker.get(WorkerFields.KEYS_READ),
+                        worker.get(WorkerFields.KEYS_WRITTEN)
+                );
+            }
+        });
+
         writer.println("</tbody>");
         writer.println("</table>");
         writer.println("</body></html>");
@@ -60,12 +80,19 @@ public class MasterServlet extends HttpServlet {
         WorkerFields.requestsParams.forEach(param -> {
             workerStatus.put(param, request.getParameter(param));
         });
-        normalize(workerStatus);
+
+        boolean success = normalize(workerStatus);
+        if (success) {
+            String workerId = (String) workerStatus.get(WorkerFields.IP + ":" + WorkerFields.PORT);
+            workerStatus.put(WorkerFields.ID, workerId);
+            workers.put(workerId, workerStatus);
+        }
     }
 
-    private void normalize(HashMap<String, Object> workerStatus) throws IOException {
+    private boolean normalize(HashMap<String, Object> workerStatus) throws IOException {
         if (workerStatus.values().stream().anyMatch(value -> value == null)) {
             response.sendError(400, "Usage: parameters port, status, job, keysRead, keysWritten, results required");
+            return false;
         }
 
         try {
@@ -75,6 +102,8 @@ public class MasterServlet extends HttpServlet {
             });
         } catch (NumberFormatException e) {
             response.sendError(400, "`port`, `keysRead`, `keysWritten` parameters must be numbers");
+            return false;
         }
+        return true;
     }
 }
