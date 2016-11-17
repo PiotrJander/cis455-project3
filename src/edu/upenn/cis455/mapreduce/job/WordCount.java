e6f2b51ff3b3f190ebe71375f70aefcbdf711e28
PiotrJander <pjander2@gmail.com>
2016-11-16 19:34:27 -0500

    Impls WordCount job

--- src/edu/upenn/cis455/mapreduce/job/WordCount.java
+++ src/edu/upenn/cis455/mapreduce/job/WordCount.java
@@ -1,24 +1,25 @@
 package edu.upenn.cis455.mapreduce.job;
 
-import java.util.Iterator;
-
 import edu.upenn.cis455.mapreduce.Context;
 import edu.upenn.cis455.mapreduce.Job;
 
+import java.util.Iterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class WordCount implements Job {
 
-  public void map(String key, String value, Context context)
-  {
-    // Your map function for WordCount goes here
+    public void map(String key, String value, Context context) {
 
+        context.write(value, "1");
+    }
 
-  }
-  
-  public void reduce(String key, Iterator<String> values, Context context)
-  {
-    // Your reduce function for WordCount goes here
+    public void reduce(String key, Iterator<String> values, Context context) {
 
+        Iterable<String> iterable = () -> values;
+        Stream<String> stream = StreamSupport.stream(iterable.spliterator(), false);
+        int sum = stream.mapToInt(Integer::parseInt).sum();
+        context.write(key, String.valueOf(sum));
+    }
 
-  }
-  
 }
